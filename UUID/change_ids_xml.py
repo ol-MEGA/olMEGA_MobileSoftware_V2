1bf94d2e58859a82ba790d1bf5376b4c32430d0f
Ulrik Kowalk <ulrik.kowalk@jade-hs.de>
2022-02-08 18:11:36 +0100

    XML parser works

--- UUID/change_ids_xml.py
+++ UUID/change_ids_xml.py
@@ -7,54 +7,77 @@ import xml.etree.ElementTree as ET
 
 uuids = {}
 
+
 def main():
+    """Function that replaces all ids and corresponding filters in xml questionnaire by UUIDs
+
+    UUIDs are Universally Unique Identifiers, which exist only in one instance and therefore represent a secure means to
+    individually classify a given entity. They are used as keys to the olMEGA database, so no two questions may be saved
+    under the same key.
+    Your input file must contain a valid .xml structure, otherwise the script will raise an exception and report an
+    error.
+    """
+
     root = tk.Tk()
     root.withdraw()
     extension = ".xml"
-    # file_path = filedialog.askopenfilename(title="Please select questionnaire *.xml file", filetypes=(("Quest files", extension),))
-    file_path = "E:\olMEGA_MobileSoftware_V2\quest\HAPPAA_01-1_21-04-15.xml"
+    file_path = filedialog.askopenfilename(title="Please select questionnaire *.xml file", filetypes=(("Quest files", extension),))
 
     open(f"{os.path.splitext(file_path)[0]}_UUID{extension}", 'w').close()
     file_path_new = f"{os.path.splitext(file_path)[0]}_UUID{extension}"
 
     print(f"File selected: {file_path}")
 
-    f_original = open(file_path, "r")
-    lines = f_original.readlines()
-    f_original.close()
+    try:
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+
+        # find and replace option ids - necessary to do first because option ids might include question ids in string
+        for item in root.iter('option'):
+            item.attrib['id'] = put_and_get_id(item.attrib['id'])
+
+        # find and replace ids in question head
+        for child in root:
+
+            # Find all filters and store them in uuid dict
+            if 'filter' in child.attrib:
+                filter_list = child.attrib['filter'].split(',')
+                for filter in filter_list:
+                    filter_stripped = filter.strip()
+                    if filter_stripped.startswith('!'):
+                        put_and_get_id(filter_stripped[1:])
+                    else:
+                        put_and_get_id(filter_stripped)
+
+                # replace filter ids in questionnaire
+                filter_string = child.attrib['filter']
+                for id in uuids:
+                    filter_string = filter_string.replace(id, uuids[id])
+                child.attrib['filter'] = filter_string
+
+            # replace all option ids
+            if 'option' in child.attrib:
+                print(child.attrib['option'])
 
 
+        # replace all question ids
+        for child in root:
+            if 'id' in child.attrib:
+                child.attrib['id'] = put_and_get_id(child.attrib['id'])
+
+        tree.write(file_path_new, encoding='UTF-8', xml_declaration=True)
+
+        print(f'Success - {len(uuids)} ids were exchanged.')
+
+    except ET.ParseError as e:
+        print(f'Not a valid xml file.\nError: {e}')
 
 
-    tree = ET.parse(file_path)
-    root = tree.getroot()
+def put_and_get_id(id):
+    """Function to check whether id is already in storage and if not - create new key and uuid value
 
-    for child in root:
-        if 'id' in child.attrib:
-            child.attrib['id'] = exchange_id(child.attrib['id'])
+    """
 
-    for child in root:
-        if 'id' in child.attrib:
-            print(child.attrib['id'])
-
-        # if 'filter' in child.attrib:
-        #     print(child.attrib['filter'])
-
-
-
-
-
-    # f_new.write(new_line)
-
-    tree.write(file_path_new, encoding='UTF-8', xml_declaration=True)
-
-
-    # f_new.close()
-
-print(f'Success - {len(uuids)} ids were exchanged.')
-
-
-def exchange_id(id):
     if id not in uuids:
         # store id with UUID in dictionary
         uuids[id] = str(uuid.uuid1())
