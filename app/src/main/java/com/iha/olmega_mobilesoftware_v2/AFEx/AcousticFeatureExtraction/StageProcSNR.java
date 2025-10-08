d254b783945c65d5070875cccf0a6d062034f187
Sven Kissner <sven.kissner@jade-hs.com>
2025-10-08 11:22:52 +0200

    fix instantiation

--- app/src/main/java/com/iha/olmega_mobilesoftware_v2/AFEx/AcousticFeatureExtraction/StageProcSNR.java
+++ app/src/main/java/com/iha/olmega_mobilesoftware_v2/AFEx/AcousticFeatureExtraction/StageProcSNR.java
@@ -19,6 +19,12 @@ public class StageProcSNR extends Stage {
     }
 
     @Override
+    void start(){
+        snr = new SNR();
+        super.start();
+    }
+
+    @Override
     void rebuffer() {
         // we are getting passed through data so we have both, the audio and VAD data. We do not want
         // to rebuffer that (for now), so the blocksize of this stage must match the incoming stage,
@@ -41,35 +47,33 @@ public class StageProcSNR extends Stage {
 
     @Override
     protected void process(float[][] buffer) {
-        SNR snr = new SNR();
         snr.calculate(buffer);
     }
 
     private class SNR {
 
         float[][] snr_value = new float[1][1];
-        float[] rms_speech = new float[2];
-        float[] rms_noise = new float[2];
+        float rms_speech = 0.0f;
+        float rms_noise = 0.0f;
 
         SNR() {
             snr_value[0][0] = 0.0f;
-            rms_speech[0] = 0.0f;
-            rms_speech[1] = 0.0f;
-            rms_noise[0] = 0.0f;
-            rms_noise[1] = 0.0f;
         }
 
         void calculate(float[][] input) {
             boolean isSpeech = input[input.length - 1][0] == 1.0f;
-            if (isSpeech) { // update speech rms and hold noise
-                rms_speech[1] = rms(input[0]);
-                rms_noise[1] = rms_noise[0];
-            } else {  // update noise rms and hold speech
-                rms_noise[1] = rms(input[0]);
-                rms_speech[1] = rms_speech[0];
+            if (isSpeech) { // update speech rms
+                rms_speech = rms(input[0]);
+            } else {  // update noise rms
+                rms_noise = rms(input[0]);
             }
-            snr_value[0][0] = rms_speech[1] / (rms_noise[1] + 1e-6f);
-            send(snr_value);
+            snr_value[0][0] = rms_speech / (rms_noise + 1e-6f);
+            float[][] tmp_out = new float[1][3];
+            tmp_out[0][0] = snr_value[0][0];
+            tmp_out[0][1] = rms_speech;
+            tmp_out[0][2] = rms_noise;
+            send(tmp_out);
+            //send(snr_value);
         }
     }
 
