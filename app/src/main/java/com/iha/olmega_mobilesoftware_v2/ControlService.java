504fd881b68151b043d910c5284a099e4d9a4704
Sven Kissner <sven.kissner@jade-hs.com>
2025-12-18 15:08:46 +0100

    test fixes for AudioRecord init on USB with locked device

--- app/src/main/java/com/iha/olmega_mobilesoftware_v2/ControlService.java
+++ app/src/main/java/com/iha/olmega_mobilesoftware_v2/ControlService.java
@@ -7,7 +7,6 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
